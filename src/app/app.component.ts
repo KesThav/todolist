6fbbae1fbf6e03c2a83127424aab984b5f30c018
KesThav <kesigan.thav@gmail.com>
2022-02-13 14:38:20 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todolist';
+export class AppComponent{
+
+  taskList: Array<string> = [];
+  task!: string;
+
+  addToList(task: string) {
+    console.log(task);
+    if (task !== "" && task !== undefined) {
+    this.taskList.push(this.task);
+    this.task = ""
+    }
+
+  }
+  
+  removeFromList(index: number) {
+    delete this.taskList[index];
+    this.taskList = this.taskList.filter(data => data !== null)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    TaskComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
